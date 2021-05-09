ec5a832564040b4107595b9084b590bd32ba1403
abubaca4 <abubaca4@gmail.com>
2021-05-09 23:30:50 +0300

    lab 6 done

--- Lab6/SinToFile.java
+++ Lab6/SinToFile.java
@@ -39,5 +39,19 @@ public class SinToFile {
         else {
             System.out.println("Error, unable to convert data");
         }
+
+        ObjectOutputStream objectOutputStream2 = new ObjectOutputStream(new FileOutputStream("./Lab6/sin2.dat"));
+        for (Double double1 : data) {
+            objectOutputStream2.writeDouble(double1);
+        }
+        objectOutputStream2.close();
+
+        Vector<Double> data_input2 = new Vector<Double>();
+        ObjectInputStream objectInputStream2 = new ObjectInputStream(new FileInputStream("./Lab6/sin2.dat"));
+        while (objectInputStream2.available() > 0) {
+            data_input2.add(objectInputStream2.readDouble());
+        }
+        objectInputStream2.close();
+        System.out.println(data_input2);
     }
 }
